ff023f5ce98be868428e31dfb837d65249d57f3c
Tom MacWright <tom@macwright.org>
2011-09-29 18:09:10 -0400

    Design, functionality updates

--- js/site.js
+++ js/site.js
@@ -1,7 +1,7 @@
 var map;
 var mm = com.modestmaps;
 $.domReady(function() {
-    wax.tilejson('http://tiles.mapbox.com/tmcw/api/Tileset/superfund_48b16e', function(tj) {
+    wax.tilejson('http://tiles.mapbox.com/tmcw/api/Tileset/superfund_1af9ac', function(tj) {
         map = new mm.Map('map',
             new wax.mm.connector(tj),
             null, [
@@ -14,6 +14,28 @@ $.domReady(function() {
         wax.mm.zoombox(map);
         wax.mm.interaction(map, tj, {
             animationOut: 'hide'
+        });
+    });
+
+    $('#butte').click(function(e) {
+        e.preventDefault();
+        easey.slow(map, {
+            zoom:8,
+            location: new mm.Location(
+                46.07953676396906,
+                -112.68265950280083)
+        });
+    });
+
+
+    $('#lovecanal').click(function(e) {
+        e.preventDefault();
+        easey.slow(map, {
+            zoom:8,
+            location: new mm.Location(
+                43.06919439483221,
+                -78.93251611607292
+            )
         });
     });
 });
