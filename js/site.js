2c4be51de1a003028233c201d3b8d3ac367a0d70
Tom MacWright <tom@macwright.org>
2011-09-30 00:53:57 -0400

    More machinery

--- js/site.js
+++ js/site.js
@@ -1,6 +1,30 @@
 var map;
 var mm = com.modestmaps;
 $.domReady(function() {
+
+    function gridQuery(l) {
+      utfgridquery('http://d.tiles.mapbox.com/tmcw/1.0.0/superfundvoronoi/layer.json', {
+        lat: l.lat,
+        lon: l.lon
+        }, function(f) {
+            $('.your').html('<em>your superfund</em> <h2>' + f.FAC_NAME + ', <small>/' + f.LOC_CITY + ', ' + f.LOC_STATE + '</small></h3>');
+            easey.slow(map, {
+                location: new mm.Location(l.lat, l.lon),
+                zoom: 9,
+                time: 5000
+            });
+        });
+    }
+
+    if (navigator && navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(res) {
+          gridQuery({
+            lat: res.coords.latitude,
+            lon: res.coords.longitude
+          });
+        });
+    }
+
     wax.tilejson('http://a.tiles.mapbox.com/tmcw/1.0.0/tmcw.superfund_1af9ac,mapbox.world-glass/layer.json', function(tj) {
         map = new mm.Map('map',
             new wax.mm.connector(tj),
